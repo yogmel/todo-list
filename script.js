6d13978f13dc86795c37225710fb917e5c18f5d0
Mellina Yonashiro <mellinay@yousea.me>
2019-01-31 22:51:42 -0200

    Drag and Drop list items

--- script.js
+++ script.js
@@ -19,15 +19,18 @@ $todoForm.addEventListener('submit', (e) => {
     removeTarefa.className = "todo__lista-remove";
     removeTarefa.innerHTML = "remover";
     item.className = "todo__lista";
+    item.setAttribute('draggable', true);
     tarefa.innerHTML = input;
-
+    
     item.appendChild(tarefa);
     item.appendChild(removeTarefa);
-
-
-    //tarefa.appendChild(removeTarefa);
+    
     $todoList.appendChild(item);
-  
+
+    // item.setAttribute('ondragstart', 'dragStartHandler(e)');
+    // item.setAttribute('ondragover', 'dragOverHandler(e)');
+    // item.setAttribute('ondrop', 'dragSDropHandler(e)');
+    
     $todoItem = document.querySelectorAll('.todo__lista');
     $todoInput.value = '';
   }
@@ -67,3 +70,35 @@ $todoToggleAll.addEventListener('click', () => {
 $todoRemoverTodos.addEventListener('click', () => {
   $todoList.innerHTML = '';
 })
+
+$todoList.addEventListener('dragstart', (e) =>{
+  console.log(e.target);
+  dragStartHandler(e);
+})
+
+function dragStartHandler(ev) {
+  ev.dataTransfer.setData("text/plain", ev.target.innerHTML);
+ ev.dropEffect = "move";
+}
+
+$todoList.addEventListener('ondragover', (e) =>{
+  console.log('droping area', e.target);
+})
+
+$todoList.addEventListener('drop', (e) => {
+  console.log(e.target);
+})
+
+
+// Drag and Drop event handlers
+function dragStartHandler(e){
+  console.log(e.target);
+}
+
+function dragOverHandler(e){
+  console.log(e.target);
+}
+
+function dragDropHandler(e){
+  console.log(e.target);
+}
